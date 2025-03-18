27e17960f7add85020b245cac6577ba2d36709cd
k-Knight <28106261+k-Knight@users.noreply.github.com>
2025-03-18 17:35:10 +0200

    added project files

---
+++ EasierSkillCurve.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using static Skills;
+
+namespace MyBepInExPlugin
+{
+    [BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        const string pluginGUID = "EasierSkillCurve";
+        const string pluginName = "EasierSkillCurve";
+        const string pluginVersion = "0.1.0";
+
+        private readonly Harmony HarmonyInstance = new Harmony(pluginGUID);
+
+        public static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(pluginName);
+
+        public void Awake() {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+        }
+
+        // More Code Here!
+        [HarmonyPatch(typeof(Skill), "GetNextLevelRequirement")]
+        public static class Patch_Skill_GetNextLevelRequirement {
+            public static bool Prefix(Skill __instance, ref float __result) {
+                __result = Mathf.Floor(__instance.m_level + 1f);
+                return false;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EasierSkillCurve
-Makes required skill experience curve linear in Valheim
+Makes required skill experience curve linear in Valheim.
+
+As of right now the formula for required experience to get next skill level in Valheim grows exponentially. This mod makes it so it increases linearly starting from 1 experience for level 1 and increasing by 1 each level.
